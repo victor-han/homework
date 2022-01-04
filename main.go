9da282c258a7ecca47bb91e25e15e7922c3738ce
“victor” <247073897@qq.com>
2022-01-04 19:16:54 +0800

    init

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+
+import(
+
+)
+
+
+
+//区块链结构提
+type Block struct{
+	PreHash string
+	Hash 	string
+	Nonce 	string
+	data 	string
+	Index	int
+	Timestamp string
+}
+
+var Blockchain []Block
+
+func main(){
+	
+}
